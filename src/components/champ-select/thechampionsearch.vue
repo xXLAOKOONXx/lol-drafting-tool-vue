eca7ef428d2b7796a26c35199b64d560909b4a52
Tom Eckstein <tom.eckstein@web.de>
2020-11-25 13:20:46 +0100

    Add Searchbar and hide maéchanism for champion selection

--- src/components/champ-select/thechampionsearch.vue
+++ src/components/champ-select/thechampionsearch.vue
@@ -1,17 +1,41 @@
 <template>
-  <div id='championsearchwrapper'>
-    <div class='champion' v-for="champ in championlist" :key="champ.id" @click="championclicked($event, champ.id)">
+<div id='mid'>
+  <button @click="hidden = !hidden" />
+  <span v-if="!hidden">
+    <span>
+      <input v-model="searchtext" placeholder="Search"/>
+    </span>
+  </span>
+  <div id='championsearchwrapper' v-if="!hidden">
+    <div
+    class='champion'
+    v-for="champ in filteredList"
+    :key="champ.id"
+    @click="championclicked($event, champ.id)">
       <img :src="champ.tileurl" />
     </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
+  data: function () {
+    return {
+      searchtext: '',
+      hidden: false
+    }
+  },
   props: {
     championlist: {
       type: Array,
       required: true
+    }
+  },
+  computed: {
+    filteredList () {
+      if (this.searchtext === '') return this.championlist
+      return this.championlist.filter(champ => champ.name.toLowerCase().includes(this.searchtext.toLowerCase()))
     }
   },
   methods: {
